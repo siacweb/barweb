26255be2343c5e6cc7fae918331bdec3608de4aa
siac web <siac.bar.web@gmail.com>
2013-09-01 01:29:21 -0300

    Criaçao de admin da página inicial (principal)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130831215620) do
+ActiveRecord::Schema.define(:version => 20130901041730) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,5 +45,36 @@ ActiveRecord::Schema.define(:version => 20130831215620) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "principals", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "main_image_file_name"
+    t.string   "main_image_content_type"
+    t.integer  "main_image_file_size"
+    t.datetime "main_image_updated_at"
+    t.string   "gallery_image_file_name"
+    t.string   "gallery_image_content_type"
+    t.integer  "gallery_image_file_size"
+    t.datetime "gallery_image_updated_at"
+    t.string   "events_image_file_name"
+    t.string   "events_image_content_type"
+    t.integer  "events_image_file_size"
+    t.datetime "events_image_updated_at"
+    t.string   "contact_image_file_name"
+    t.string   "contact_image_content_type"
+    t.integer  "contact_image_file_size"
+    t.datetime "contact_image_updated_at"
+    t.string   "follow_fb_image_file_name"
+    t.string   "follow_fb_image_content_type"
+    t.integer  "follow_fb_image_file_size"
+    t.datetime "follow_fb_image_updated_at"
+    t.string   "follow_tw_image_file_name"
+    t.string   "follow_tw_image_content_type"
+    t.integer  "follow_tw_image_file_size"
+    t.datetime "follow_tw_image_updated_at"
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+  end
 
 end
