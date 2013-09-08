af7e1686e8e7e8173d63217a32d5355535b27ec2
siac web <siac.bar.web@gmail.com>
2013-09-08 15:44:01 -0300

    correção e otimização da busca de bares

--- app/controllers/home_bar_controller.rb
+++ app/controllers/home_bar_controller.rb
@@ -1,13 +1,15 @@
 class HomeBarController < ApplicationController
 
   def index
-    #TODO: BUSCAR PELO NOME DO BAR - APENAS NOME QUE BATE COM O QUE ESTÁ SENDO PROCURADO
-    #SÓ TEM Q CARREGAR O Q A PESSOA CLICOU NA PAGINA INICIAL,
-    # N IMPORTA C POR ID OU NOME, MAS NA URL DEVE APARECER O NOME, SO ..
-    @home_bar = HomeBar.first
+
+    #@home_bar = HomeBar.first
 
     #@home_bar = HomeBar.find_all_by_name(get_current_bar_name)
-    #@home_bar = HomeBar.find_all_by_name(params[:name]).first
+
+    #FUNCIONA!
+    @home_bar = HomeBar.find_by_name(params[:name])
+    #@home_bar = HomeBar.find(params[:name])
+
   end
 
 end
--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -4,6 +4,14 @@ class PrincipalController < ApplicationController
     #TODO: buscar só a página ativa
     @principals = Principal.all
 
+    #TODO: selecionar soh alguns bares (de forma aleatoria ou não)
+    @home_bars = HomeBar.all
+
+    #@home_bar = HomeBar.find(params[:name])
+
+    @home_bar = HomeBar.find_by_name(get_current_bar_name)
+    #@home_bar = HomeBar.find_by_name(params[:name])
+
   end
 
   def contact
